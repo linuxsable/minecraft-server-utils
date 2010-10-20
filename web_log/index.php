4b794f5319b2e43d4ea94cad4952292161687987
Tyler Van Hoomissen <linuxsable@gmail.com>
2010-10-19 23:58:35 -0700

    removed ips

--- web_log/index.php
+++ web_log/index.php
@@ -1,20 +1,22 @@
 <html>
-    <head>
-        <style type="text/css" media="screen">
-            body {font-family: courier; font-size: 12px;}</style>
-        </style>
-    </head>
-    <body>
+<head>
+    <style type="text/css" media="screen">
+        body {font-family: courier; font-size: 12px;}</style>
+    </style>
+</head>
+<body>
     <?
-        echo '<p><strong>Server Uptime:';
-        echo `uptime`;
-        echo '</strong></p>';
+    echo '<p><strong>Server Uptime:';
+    echo `uptime`;
+    echo '</strong></p>';
 
-        $file_contents = file('/home/dm/minecraft/server.log');
-        $file_contents = array_reverse($file_contents);
-        foreach ($file_contents as $line) {
-                echo $line . '<br/>';
-        }
+    $file_contents = file('/home/dm/minecraft/server.log');
+    $file_contents = array_reverse($file_contents);
+    foreach ($file_contents as $line) {
+        $line = preg_replace('/([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/', 'PRIVATE-IP', $line);
+        $line = str_replace('[INFO]', '', $line);
+        echo htmlspecialchars($line) . '<br/>';
+    }
     ?>
-    </body>
+</body>
 </html>
