873da31d158cabe51debb1c7c517c23955b92467
Tyler Van Hoomissen <linuxsable@gmail.com>
2012-08-12 11:13:38 -0700

    Update web_log/index.php
    Truncate output to 500 lines.

--- web_log/index.php
+++ web_log/index.php
@@ -5,18 +5,21 @@
     </style>
 </head>
 <body>
-    <?
+    <?php
+    
     echo '<p><strong>Server Uptime:';
     echo `uptime`;
     echo '</strong></p>';
 
     $file_contents = file('/home/dm/minecraft/server.log');
     $file_contents = array_reverse($file_contents);
+    $file_contents = array_slice($file_contents, 0, 500);
     foreach ($file_contents as $line) {
         $line = preg_replace('/([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/', 'PRIVATE-IP', $line);
         $line = str_replace('[INFO]', '', $line);
         echo htmlspecialchars($line) . '<br/>';
     }
+    
     ?>
 </body>
 </html>
